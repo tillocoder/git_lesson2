746952755fb14437086ec49067dce57ac6fee6df
tillo <tillo4079@gmail.com>
2024-06-25 02:03:35 +0500

    new add function created

--- bin/git_2.dart
+++ bin/git_2.dart
@@ -1,6 +1,9 @@
 import 'package:git_2/git_2.dart' as git_2;
+import 'index.dart';
 
 void main(List<String> arguments) {
+  git_2.calculate();
+  calculate(112, 212);
   print('Hello world: ${git_2.calculate()}!');
   print('Hello world: ${git_2.calculate()}!');
   print('Hello world: ${git_2.calculate()}!');
---
+++ bin/index.dart
@@ -0,0 +1,3 @@
+int calculate(int a, int v) {
+  return a + v;
+}
